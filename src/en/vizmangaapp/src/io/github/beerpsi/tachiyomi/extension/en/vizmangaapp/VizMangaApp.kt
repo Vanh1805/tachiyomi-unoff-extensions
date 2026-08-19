679b10999a8198adba47e59a35ad7e9e07b8ef33
Yomi <59566412+Vanh1805@users.noreply.github.com>
2026-08-19 08:35:59 +0000

    remove some extension

--- src/en/vizmangaapp/src/io/github/beerpsi/tachiyomi/extension/en/vizmangaapp/VizMangaApp.kt
+++
@@ -1,9 +0,0 @@
-// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
-@file:Suppress("ktlint")
-
-package io.github.beerpsi.tachiyomi.extension.en.vizmangaapp
-
-import io.github.beerpsi.tachiyomi.multisrc.viz.Viz
-
-
-class VizMangaApp : Viz("VIZ Manga (App)", "https://www.viz.com/vizmanga", "en")
